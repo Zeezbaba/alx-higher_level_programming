4994a17e32058be37e9d72119eff8de15801fdec
Zeezbaba <Azeez_okeleji>
2023-11-08 16:57:17 -0800

    102-complex_delete.py

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    new_dict = [key for key, val in a_dictionary.items() if val == value]
+
+    for key in new_dict:
+        del a_dictionary[key]
+
+    return a_dictionary
