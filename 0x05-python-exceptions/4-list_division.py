53344e891afd03924c9442cc90f681903cd6f7d2
Zeezbaba <Azeez_okeleji>
2023-11-20 06:51:03 -0800

    4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def list_division(my_list_1, my_list_2, list_length):
     result_list = []
     for i in range(0, list_length):
@@ -15,5 +14,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             result = 0
         finally:
-            result_list.append[result]
+            result_list.append(result)
     return (result_list)
