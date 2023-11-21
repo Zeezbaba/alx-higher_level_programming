a24c66860424ad964b1ee3a080d6227478350fbf
Zeezbaba <Azeez_okeleji>
2023-11-21 03:33:18 -0800

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-#square class with attribute by Zeezbaba
+# square class with attribute by Zeezbaba
 
 class Square:
+    """square class"""
     def __init__(self, size):
-        """Initiating the square class"""
+        """Initiating the square class
+        args: size is the size of the square given"""
+
         self.__size = size
