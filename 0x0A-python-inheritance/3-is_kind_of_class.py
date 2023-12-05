555517aa6dc95880e00b9c60bf104939a9bb1dd8
Zeezbaba <Azeez_okeleji>
2023-12-04 16:07:07 -0800

    3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-    Function to check if an object is instance of a class
-"""
+"""Function to check if an object is instance of a class"""
 
 
 def is_kind_of_class(obj, a_class):
