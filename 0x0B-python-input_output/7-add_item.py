8f3c5dcb190e3b52db67378134c6e70fe8232959
Zeezbaba <Azeez_okeleji>
2023-12-05 16:17:52 -0800

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Module that adds all arguments to a Python list, and then save them to a file"""
+"""
+    Module that adds all arguments to a Python list,
+    and then save them to a file
+"""
 
 
 import sys
