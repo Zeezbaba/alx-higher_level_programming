b67bffa3a549e2f6059f68d1c0b3b420047388bd
Zeezbaba <Azeez_okeleji>
2024-02-12 12:10:14 -0800

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const argCount = process.argv.length;
+const argCount = process.argv.length - 2;
 
-if (argCount > 2) {
-	console.log('Argument found');
-} else if (argCount > 3) {
-	console.log('Arguments found');
-} else {
+if (argCount === 0) {
 	console.log('No argument');
+} else if (argCount === 1) {
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
 }
