bd2aae4965c418c095797301d4d6b2e29e5a895d
Zeezbaba <Azeez_okeleji>
2024-02-12 14:41:53 -0800

    Second largest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 function secondMax (integers) {
-   const arrangedInt = integers.sort((a, b) => b - a);
-    return arrangedInt[1] || 0;
+  const arrangedInt = integers.sort((a, b) => b - a);
+  return arrangedInt[1] || 0;
 }
 
 const argument = process.argv.slice(2).map(Number);
 
 if (argument.length <= 1) {
-    console.log(0);
+  console.log(0);
 } else {
-    console.log(secondMax(argument));
+  console.log(secondMax(argument));
 }
