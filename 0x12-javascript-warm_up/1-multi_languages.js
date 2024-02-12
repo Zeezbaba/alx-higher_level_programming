3046c8def15f601d6f7b81c609a9a3ff879ad7de
Zeezbaba <Azeez_okeleji>
2024-02-12 11:43:33 -0800

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const firstLine = 'C is fun';
+let secondLine = 'Python is cool';
+let thirdLine = 'JavaScript is amazing';
+
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
