2ab76856de5f9133a8c1975e57d9814b3508d274
Zeezbaba <Azeez_okeleji>
2024-02-13 15:53:27 -0800

    Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && Number.isInteger(w) && h > 0 && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    } else {
+      this.width = 0;
+      this.height = 0;
+    }
+  }
+}
+
+module.exports = Rectangle;
