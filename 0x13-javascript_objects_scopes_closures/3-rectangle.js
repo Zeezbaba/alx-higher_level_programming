869161311528a4f558fab10fac8d657c003ce4a0
Zeezbaba <Azeez_okeleji>
2024-02-13 18:25:43 -0800

    Rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -5,9 +5,6 @@ class Rectangle {
     if (w > 0 && Number.isInteger(w) && h > 0 && Number.isInteger(h)) {
       this.width = w;
       this.height = h;
-    } else {
-      this.width = 0;
-      this.height = 0;
     }
   }
 
