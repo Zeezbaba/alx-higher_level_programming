9c96b167150114a64522218e8ed38bdf7018c805
Zeezbaba <Azeez_okeleji>
2024-02-17 14:25:16 -0800

    City relationship

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -16,15 +16,13 @@ if __name__ == "__main__":
     db = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         argv[1], argv[2], argv[3])
     engine = create_engine(db)
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
 
     session = Session()
 
-    california = State(name='California')
-    san_francisco = City(name='San Francisco')
-    california.cities.append(san_francisco)
-
+    california = State(name="California", cities=[City(name="San Francisco")])
     session.add(california)
     session.commit()
     session.close()
