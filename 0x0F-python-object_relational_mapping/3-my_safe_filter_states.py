910a9e7c3e0505a0e5058df5a4b33dec60e6cd78
Zeezbaba <Azeez_okeleji>
2024-02-16 18:33:36 -0800

    SQL Injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,20 +5,21 @@ all values in the states table of hbtn_0e_0_usa
 where name matches the argument
 """
 
-import MySQLdb as myDb
+import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """
     Accessing the database
     """
-    db = myDb.connect(
+    db = MySQLdb.connect(
         host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
 
     curs = db.cursor()
 
     query = "SELECT * FROM states WHERE name LIKE \
-        BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]}
+            BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]}
+
     curs.execute(query)
 
     states = curs.fetchall()
